7b40837cb6f047d1bea3fb677c1e0ffc8474836c
Aurnab Barua <aurnabbarua02@gmail.com>
2024-11-05 12:55:40 +0600

    Update files

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleColumn() {
+    var table = document.getElementById("myTable");
+    var button = document.getElementById('btn');
+    var isHidden = table.rows[0].cells[2].classList.contains("hidden");
+    for (var i = 0; i < table.rows.length; i++) {
+        table.rows[i].cells[2].classList.toggle("hidden");
+    }
+    button.textContent = isHidden? 'Hide Meaning': 'Show Meaning';
+} 
