20ad97947493004b12310477fb1a30868514f9ec
Aurnab Barua <aurnabbarua02@gmail.com>
2024-11-12 12:07:20 +0600

    Update files

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ function toggleColumn() {
     }
     button.textContent = isHidden ? 'Hide Meaning' : 'Show Meaning';
 }
- 
+function navbar() {
+    const pages = [
+        { href: 'index.html', text: 'Page 1 (1-100)' }, 
+        { href: 'page2.html', text: 'Page 2 (101-200)' }, 
+        { href: 'page3.html', text: 'Page 3 (201-300)' }, 
+        { href: 'page4.html', text: 'Page 4 (301-400)' }, 
+        { href: 'page5.html', text: 'Page 5 (401-500)' }, 
+        { href: 'page6.html', text: 'Page 6 (501-600)' }, 
+        { href: 'page7.html', text: 'Page 7 (601-700)' }, 
+        { href: 'page8.html', text: 'Page 8 (701-800)' }]; 
+        const navbarList = document.getElementById('navbar-list'); 
+        pages.forEach((page, index) => { 
+            const li = document.createElement('li'); 
+            li.className = 'nav-item' + (index === 0 ? ' active' : ''); 
+            const a = document.createElement('a'); 
+            a.className = 'nav-link'; 
+            a.href = page.href; 
+            a.textContent = page.text; 
+            li.appendChild(a); 
+            navbarList.appendChild(li); 
+        });
+}
+
